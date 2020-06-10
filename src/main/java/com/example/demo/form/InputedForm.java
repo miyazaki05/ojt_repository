e3fb4dc3fb47783165092589127f1eeeacc8e36a
miyazaki05 <miyazaki@kmkworld.com>
2020-06-10 14:29:57 +0900

    登録画面修正版1(未完成) ModelAndViewでの画面の遷移は成功、メッセージの表示を失敗

---
+++ src/main/java/com/example/demo/form/InputedForm.java
@@ -0,0 +1,20 @@
+package com.example.demo.form;
+
+public class InputedForm {
+	private String name;
+	private String phoneNumber;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- src/main/java/com/example/demo/service/RegistLogic.java
+++ src/main/java/com/example/demo/service/RegistLogic.java
@@ -25,6 +25,8 @@ public class RegistLogic {
 			phoneBookRepository.regist(name, phoneNumber);
 		}
 
+		mav.setViewName("regist");
+
 	}
 
 }
